685b6de102c09c102816ad6e195f7338cdba489f
Aravind <aravind1985@gmail.com>
2020-07-06 21:33:36 +0530

    Updated files

---
+++ tests/consumer.pact.js
@@ -0,0 +1,46 @@
+const path = require("path")
+const chai = require("chai")
+const { Pact, Matchers } = require("@pact-foundation/pact")
+const chaiAsPromised = require("chai-as-promised")
+const { string } = require("@pact-foundation/pact/dsl/matchers")
+const expect = chai.expect
+chai.use(chaiAsPromised)
+const { string } = Matchers;
+
+describe('Consumer Test', () => {
+    const provider = new Pact({
+        consumer: "Recent",
+        provider: "token",
+        port:1234,
+        log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+        dir: path.resolve(process.cwd(), 'pacts'),
+        logLevel: "INFO"
+    });
+
+    before(() => provider.setup()
+    .then(() => provider.addInteraction({
+        state: "user token",
+        uponReceiving: "Get user token",
+        withRequest: {
+            method: "Get",
+            path: "/token/1234",
+            headers: {Accept: "application/json, text/plain, */*"}
+        },
+        willRespondWith: {
+            headers: {"Content-Type": "application/json"},
+            status: 200,
+            body: { "token": string("bearer") }
+        }
+    })))
+
+    it('OK response', () => {
+        get()
+        .then((response) => {
+            expect(response.statusText).to.be.equal('OK')
+        })
+    })
+
+    afterEach(() => provider.verify())
+
+    after(() => provider.finalize())
+})
